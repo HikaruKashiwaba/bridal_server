1cfef9647c89fd1cdfd8fd2425c419041a2d0370
igota <igota@igota-no-MacBook-Pro.local>
2018-09-20 18:28:03 +0900

    フェア登録用php修正

--- app/FairMinna.php
+++ app/FairMinna.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class FairMinna extends Model
 {
     use SoftDeletes;
-
+    
     protected $dates = ['deleted_at'];
     protected $table = 'fair_minna';
     protected $fairContent;
@@ -16,7 +16,7 @@ class FairMinna extends Model
     //protected $guarded = ['id', 'delete_flg', 'create_date', 'update_date'];
     //const CREATED_AT = 'created_at';
     //const UPDATED_AT = 'updated_at';
-
+    
     public function fair()
     {
         return $this->belongsTo('App\Fair');
